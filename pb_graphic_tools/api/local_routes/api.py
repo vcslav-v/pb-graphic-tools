38a3450a6d8df16b8aa498d050d431846d4785a1
Vcslav <vcslav.v@gmail.com>
2021-11-27 19:14:43 +0300

    test

--- pb_graphic_tools/api/local_routes/api.py
+++ pb_graphic_tools/api/local_routes/api.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, File, UploadFile
+from fastapi import APIRouter, File, UploadFile, Response
 from pb_graphic_tools.api import service
 import os
 
@@ -14,4 +14,4 @@ async def tinify(token: str, width: int = None, files: list[UploadFile] = File(.
         tinified_zip = await service.tinify_imgs(files, width)
     except ValueError as val_err:
         return {'error': val_err.args}
-    return tinified_zip
+    return Response(content=tinified_zip, media_type='application/zip')
